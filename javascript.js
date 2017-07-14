75519edd03b1ba748e8a3538b6abcca3e2209a01
peterstinner <peter.stinner@gmail.com>
2017-07-14 16:44:05 +0200

    ProbierMal

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 function openWin() {
-    window.open("https://www.w3schools.com","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+     window.open("https://www.w3schools.com","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
 }
